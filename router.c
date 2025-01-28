377652bcd9827ea02fe4c004cc3350cda8913238
Yotam Greenstein <yotamgr314@gmail.com>
2025-01-28 17:37:11 +0200

    skeletons files provided by Eran

---
+++ router.c
@@ -0,0 +1,42 @@
+#include "httpd.h"
+#include <unistd.h>
+#include <stdio.h>
+#include<stdio.h> 
+#include<unistd.h> 
+#include<fcntl.h> 
+#include<stdlib.h> 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#define BUF_SIZE 1024
+
+void route()
+{
+    ROUTE_START()
+
+    ROUTE_GET("/")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Hello! You are using %s", request_header("User-Agent"));
+    }
+
+    ROUTE_GET("/pass")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Hello! you got here %s , %s %s",method,uri,qs);
+    }
+
+    ROUTE_POST("/")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Wow, seems that you POSTed %d bytes. \r\n", payload_size);
+        printf("Fetch the data using `payload` variable.");
+    }
+  
+    ROUTE_END()
+}
